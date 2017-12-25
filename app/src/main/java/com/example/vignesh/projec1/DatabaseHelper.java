2e35a611bfb52a160e39e12e6acca99950236034
vignesh5698 <vigneshit13@gmail.com>
2017-12-25 10:39:20 +0530

    SignUp and Login using SQLite dB

--- app/src/main/java/com/example/vignesh/projec1/DatabaseHelper.java
+++ app/src/main/java/com/example/vignesh/projec1/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.vignesh.projec1;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -19,7 +20,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     private static final String col_uname="uname";
     private static final String col_pass="pass";
     SQLiteDatabase db;
-    private final String table_create="create table "+table_name+" (id integer , name text);";
+    private final String table_create="create table "+table_name+" (id integer , uname text);";
 
     public DatabaseHelper(Context context){
         super(context,db_name,null,db_ver);
@@ -38,6 +39,26 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         contentValues.put(col_pass,user.getPass());
 
         db.insert(table_name,null,contentValues);
+        db.close();
+    }
+    public String searchPass(String pass){
+        db=this.getWritableDatabase();
+        String query="select uname,pass from "+table_name;
+        Cursor cursor=db.rawQuery(query,null);
+        String a,b;
+        a="not found";
+        if(cursor.moveToFirst()){
+            do{
+
+                b=cursor.getString(1);
+                if(b.equals(pass)){
+                    a=cursor.getString(0);
+                    break;
+                }
+            }while (cursor.moveToNext());
+
+        }
+        return a;
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
--- app/src/main/java/com/example/vignesh/projec1/MainActivity.java
+++ app/src/main/java/com/example/vignesh/projec1/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.vignesh.projec1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.mock.MockApplication;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
 
     Button button;
+    DatabaseHelper databaseHelper=new DatabaseHelper(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         button=(Button)findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        login=(Button)findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText uname1=(EditText)findViewById(R.id.et1);
+                EditText pass1=(EditText)findViewById(R.id.et2);
+                String uname=uname1.getText().toString();
+                String pass=pass1.getText().toString();
+
+                String password=databaseHelper.searchPass(pass);
+                if (pass.equals(password)){
+                    Toast.makeText(MainActivity.this,"Welcome Budyyy",Toast.LENGTH_LONG).show();
+
+                }
+                else
+                    Toast.makeText(MainActivity.this,"User Invalid",Toast.LENGTH_LONG).show();
+            }
+
+
+
+        });
+
+
     }
+    //New Way to Access button
+
+
+
+
 }
